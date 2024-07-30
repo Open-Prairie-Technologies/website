bb17b00d5250275d23e8f3fbe99ef51ef5d599cb
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2024-07-30 10:07:59 -0500

    Initial commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+	import MainNav from './MainNav.svelte'
+	import HamburgerMenuButton from './HamburgerMenuButton.svelte'
+	import { siteTitle } from '$lib/config'
+
+</script>
+
+
+<header>
+	<a href="/" class="site-title">
+		{siteTitle}
+	</a>
+	
+	<HamburgerMenuButton />
+	<MainNav />
+
+</header>
---
+++ src/routes/about/+page.md
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+# About
+
+This is the about me page. Here is where you can read all about me as a person.
