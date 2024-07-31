d1e7f2c5099a7695a4d82ecb88e6fb9bc0372320
dylan <dylan.hildenbrand@gmail.com>
2024-07-31 14:58:10 -0500

    Couldn't find a way to get TailwindCSS to work with hover:after and after:content props so did it in straight CSS.

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,25 @@
-<ul class="flex justify-start items-center gap-5">
-	<li><a href="#work">Our Work</a></li>
+<ul class="flex items-center justify-start gap-5">
+	<li>
+		<a href="#work">Our Work</a>
+	</li>
 	<li><a href="#about">About Us</a></li>
 	<li><a href="#contact">Contact</a></li>
 </ul>
+
+<style>
+	a {
+		display: inline-block;
+		transition: 250ms;
+	}
+	a::after {
+		display: block;
+		width: 0;
+		height: 1px;
+		content: '';
+		background-color: var(--color-green);
+		transition: 250ms;
+	}
+	a:hover::after {
+		width: 100%;
+	}
+</style>
