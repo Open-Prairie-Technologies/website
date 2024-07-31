a9359d70d9ba60bfa733ea8ba97f70a6254a5a6f
dylan <dylan.hildenbrand@gmail.com>
2024-07-31 15:53:09 -0500

    Consistent links across the site

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -5,22 +5,3 @@
 	<li><a href="#about">About Us</a></li>
 	<li><a href="#contact">Contact</a></li>
 </ul>
-
-<style>
-	/* TailwindCSS doesn't yet support hover on :after + :after:content  */
-	a {
-		display: inline-block;
-		transition: 250ms;
-	}
-	a::after {
-		display: block;
-		width: 0;
-		height: 1px;
-		content: '';
-		background-color: var(--color-green);
-		transition: 250ms;
-	}
-	a:hover::after {
-		width: 100%;
-	}
-</style>
