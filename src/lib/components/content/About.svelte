654180c4ebc81fc48e686f9d3297515bd1bc7d55
dylan <dylan.hildenbrand@gmail.com>
2024-08-01 14:18:52 -0500

    Instead of maintaining separate team member components, lets just have one and pass the props to that.

--- src/lib/components/content/About.svelte
+++ src/lib/components/content/About.svelte
@@ -1,7 +1,8 @@
 <script>
-	import Jimmy from '$lib/components/team/Jimmy.svelte';
-	import Jordon from '$lib/components/team/Jordon.svelte';
-	import Dylan from '$lib/components/team/Dylan.svelte';
+	import jimmy from '$lib/images/team/jimmy.jpg';
+	import jordon from '$lib/images/team/jordon.jpg';
+	import dylan from '$lib/images/team/dylan.jpg';
+	import Member from '$lib/components/team/Member.svelte';
 </script>
 
 <a
@@ -18,7 +19,36 @@
 </p>
 
 <div class="grid grid-cols-1 items-stretch gap-x-5 md:grid-cols-3">
-	<Jimmy />
-	<Jordon />
-	<Dylan />
+	<Member
+		img={jimmy}
+		name="Jimmy Hogoboom"
+		alt="Jimmy Hogoboom smiles at the camera. He has short hair, glasses, facial hair."
+		bio="
+			A software developer with over a decade of experience collaborating with clients,
+			stakeholders, and domain experts to design, build, deploy, and maintain secure web
+			applications with a variety of languages and frameworks.
+		"
+	/>
+	<Member
+		img={jordon}
+		name="Jordon Lakoduk"
+		alt="Jordon Lakoduk smiles at the camera. He is wearing a short brimmed cap and has a beard."
+		bio="
+			A qui totam quia error ipsam ea inventore. Sed beatae consequatur officiis facilis ex animi
+			id. Excepturi magni atque rerum magnam. Aut iste inventore inventore autem autem voluptatibus
+			odit. Praesentium est eveniet eius nulla facere nam voluptatum possimus. Vel exercitationem et
+			atque corporis quo.
+		"
+	/>
+	<Member
+		img={dylan}
+		name="Dylan Hildenbrand"
+		alt="Dylan Hildenbrand stands with his arms crossed. He wears a dark, long sleeve shirt, wears glasses, and has tossled hair."
+		bio="
+			Dylan Hildenbrand is a freelance full-stack web developer with a penchant for understanding
+			how things work. Over the past decade, that desire to know “how things work” has led Dylan to
+			a robust skillset ranging from front and back-end development to database administration,
+			Linux server administration, and automation of application deployments and testing procedures.
+		"
+	/>
 </div>
---
+++ src/lib/components/team/Member.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let img: string = '';
+	export let alt: string = '';
+	export let bio: string = '';
+	export let name: string = '';
+</script>
+
+<div class="grid grid-rows-2 gap-4 justify-self-start py-2">
+	<img
+		src={img}
+		class="h-52 w-52 justify-self-center rounded-full object-cover object-[center_top] py-2"
+		{alt}
+	/>
+	<span>
+		<h3 class="text-xl">{name}</h3>
+		<p>{bio}</p>
+	</span>
+</div>
