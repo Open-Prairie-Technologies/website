3c302e75b00a6ef8c57d314ec1cbb27a5e5f112d
dylan <dylan.hildenbrand@gmail.com>
2024-08-01 15:11:43 -0500

    Reduce size of logo and header on scroll

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import Nav from '$lib/components/Nav.svelte';
 	import logo from '$lib/images/logo.png';
+
+	let y: number = 0;
 </script>
 
-<div class="flex max-h-52 items-stretch justify-between p-5">
-	<img src={logo} class="h-auto max-w-36" alt="OPT Company Logo" />
+<svelte:window bind:scrollY={y} />
+<div
+	class="flex w-full items-stretch justify-between p-5 transition-all {y > 50
+		? 'max-h-24'
+		: 'max-h-52'}"
+>
+	<img
+		src={logo}
+		class="h-auto transition-all duration-200 {y > 50 ? 'max-w-16' : 'max-w-36'}"
+		alt="OPT Company Logo"
+	/>
 	<Nav />
 </div>
