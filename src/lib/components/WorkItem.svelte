1be860b41d6cf3e4a65678c1501b73d46d763248
dylan <dylan.hildenbrand@gmail.com>
2024-08-01 15:53:40 -0500

    Initial portfolio section

---
+++ src/lib/components/WorkItem.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string = '';
+	export let image: string = '';
+	export let alt: string = '';
+	export let blurb: string = '';
+</script>
+
+<div class="grid grid-cols-2">
+	<img src={image} {alt} />
+	<div class="flex flex-col">
+		<h4 class="text-xl">{title}</h4>
+		<p>{blurb}</p>
+	</div>
+</div>
--- src/lib/components/content/Work.svelte
+++ src/lib/components/content/Work.svelte
@@ -1,6 +1,23 @@
+<script lang="ts">
+	import WorkItem from '$lib/components/WorkItem.svelte';
+	import helth from '$lib/images/helthapp.png';
+	import tempo from '$lib/images/tempo.jpg';
+</script>
+
 <a
 	href="#work"
 	class="before:absolute before:ml-[-20px] before:inline-block before:text-2xl before:content-[''] hover:before:content-['#']"
 	><h2 class="text-2xl">Our Work</h2></a
 >
-<div class="flex flex-col"></div>
+<div class="flex flex-col">
+	<WorkItem
+		title="helth.app"
+		blurb="helth.app was started as a joke blah blah blah"
+		image={helth}
+	/>
+	<WorkItem
+		title="Tempo"
+		blurb="So many timer apps lock great features behind a paywall but not Tempo!"
+		image={tempo}
+	/>
+</div>
