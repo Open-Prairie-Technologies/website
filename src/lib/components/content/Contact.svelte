d837f56f4fa60e9951d3dc1bc86e63159a5cf545
dylan <dylan.hildenbrand@gmail.com>
2024-08-05 13:48:26 -0500

    Form detection + recaptcha from netlify

--- src/lib/components/content/Contact.svelte
+++ src/lib/components/content/Contact.svelte
@@ -10,13 +10,23 @@
 		your organizations needs. Send us a message aboout what you're looking for and we'll get back to
 		you ASAP!
 	</p>
-	<form class="flex flex-col" method="POST">
+	<form
+		class="flex flex-col"
+		method="POST"
+		data-netlify="true"
+		name="OPT-Contact-Form"
+		data-netlify-recaptcha="true"
+	>
+		<input name="bait" />
+		<input type="hidden" name="form-name" value="OPT-Contact-Form" />
+		<input type="hidden" name="subject" value="OPT Contact Form Submission" />
 		<label class="px-3 py-2" for="name">Name</label>
 		<input class="text-slate-800" required type="text" id="name" name="name" />
 		<label class="px-3 py-2" for="email">Email</label>
 		<input type="email" required class="text-slate-800" id="email" name="email" />
 		<label class="px-3 py-2" for="message">Message</label>
 		<textarea id="message" required class="resize-none text-slate-800" rows="4" name="message" />
+		<div data-netlify-recaptcha="true"></div>
 		<button
 			type="submit"
 			data-text="Submit"
