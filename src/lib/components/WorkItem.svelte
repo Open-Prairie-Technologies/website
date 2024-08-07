f642af60399e65a1b3a1a6e7c46f3c0a5dce431c
Dylan Hildenbrand <dylan.hildenbrand@gmail.com>
2024-08-06 20:43:46 -0500

    made some mobile fixes

--- src/lib/components/WorkItem.svelte
+++ src/lib/components/WorkItem.svelte
@@ -11,7 +11,7 @@
 		><img
 			src={image}
 			{alt}
-			class="mx-auto inline h-auto w-auto min-w-80 border-2 border-slate-300 drop-shadow-xl"
+			class="mx-auto inline h-auto w-auto border-2 border-slate-300 drop-shadow-xl"
 		/></span
 	>
 	<div class="flex flex-col">
